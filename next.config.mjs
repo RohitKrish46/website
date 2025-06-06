e6528af5bc6dc199b7fef6284514ecf13eda2677
Rohit Krishnan <krishr.somu@gmail.com>
2025-06-06 13:42:17 +0530

    made changes to deploy using github pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,46 @@
-import createMDX from '@next/mdx'
+// import createMDX from '@next/mdx'
  
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
+//   async rewrites() {
+//     return [
+//       {
+//         source: '/learning/weekly-reflections/week-:num',
+//         destination: '/learning/weekly-reflections/[week]?week=week-:num',
+//       },
+//     ]
+//   },
+// }
+ 
+// const withMDX = createMDX({
+// })
+ 
+// export default withMDX(nextConfig)
+
+import createMDX from '@next/mdx';
+
+const isProd = process.env.NODE_ENV === 'production';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
+  output: 'export', // 👈 Enable static export
+  basePath: isProd ? '/RohitKrish46/website' : '', // 👈 Update this
+  assetPrefix: isProd ? '/RohitKrish46/website' : '', // 👈 Update this
+  images: {
+    unoptimized: true, // 👈 Required for static export
+  },
   async rewrites() {
     return [
       {
         source: '/learning/weekly-reflections/week-:num',
         destination: '/learning/weekly-reflections/[week]?week=week-:num',
       },
-    ]
+    ];
   },
-}
- 
-const withMDX = createMDX({
-})
- 
-export default withMDX(nextConfig)
+};
+
+const withMDX = createMDX({});
+
+export default withMDX(nextConfig);
